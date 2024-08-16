6cce9b7a044221d07f68bee7386dc22a967e6a85
Martin Andronikos <martin.andronikos@gmail.com>
2024-08-16 19:24:16 +0300

    A Start on Main Program
    cccdn.lua has been rewritten following planned first version of the program.
    Added directory printing with adjustable color coding of entry types.

--- cccdn.lua
+++ cccdn.lua
@@ -1,42 +1,48 @@
-local function deepCopy(original)
-    if type(original) ~= "table" then
-        return original
-    end
-    local copy = {}
-    for k,v in pairs(original) do
-        copy[k] = deepCopy(v)
-    end
-    return copy
-end
-
-local function getSpeaker()
-    local speakers = { peripheral.find("speaker") }
-    if #speakers == 0 then return nil end
-    return speakers[1]
-end
-
-local function playAudio(handle)
-    local speaker = getSpeaker()
-    local decoder = require("cc.audio.dfpwm").make_decoder()
-    while true do
-        local bytes = handle.read(16384)
-        if bytes == nil then break end
-        
-        local data = decoder(bytes)
-        while not speaker.playAudio(data) do
-            os.pullEvent("speaker_audio_empty")
-        end
-    end
-    handle.close()
-    speaker.stop()
-end
-
 local lib, errtxt = require("libcdn")("/libcdn/", "main")
 if not lib then printError(errtxt) return end
+local screen = term.current()
 
-lib:addCatalog("mart-music")
-lib:changeDirectory("mart@/")
-parallel.waitForAny(
-    function () playAudio(lib:getFile("uranium-fever")) end,
-    function () while true do print("Playing") os.sleep(1) end end
-)
+local base_color = colors.blue
+local type_colors = {
+    ["cat"] = colors.blue,
+    ["dir"] = colors.lightBlue,
+    ["pwm"] = colors.lime,
+    ["lua"] = colors.yellow
+}
+
+local function printDirectory()
+    local width, height = screen.getSize()
+    local path = lib:getPath()
+    local start = #path - width + 1
+    if start > 0 then  path = string.sub(path, start, #path) end
+
+    screen.setCursorPos(1, 1)
+    screen.blit(path,
+        string.rep(colors.toBlit(base_color), #path),
+        string.rep(colors.toBlit(colors.black), #path)
+    )
+
+    local line = 2
+    local dir = lib:getDirectoryEntries()
+    for n,t in pairs(dir) do
+        n = string.sub(n, 0, width - 2)
+        screen.setCursorPos(1, line)
+        screen.blit("| ",
+            string.rep(colors.toBlit(base_color), 2),
+            string.rep(colors.toBlit(colors.black), 2)
+        )
+        screen.blit(n,
+            string.rep(colors.toBlit(type_colors[t] or colors.white), #n),
+            string.rep(colors.toBlit(colors.black), #n)
+        )
+        line = line + 1
+    end
+    screen.setCursorPos(1, height)
+end
+
+while true do
+    screen.clear()
+    printDirectory()
+    screen.write("> ")
+    read()
+end
